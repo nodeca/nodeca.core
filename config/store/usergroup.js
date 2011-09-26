bf5f38e6f0e2cf9c187f23ea4aefc95742febaf2
Aleksey V Zapparov <ixti@member.fsf.org>
2011-09-26 11:47:23 +0200

    Started reworking UG

--- config/store/usergroup.js
+++ config/store/usergroup.js
@@ -1,83 +1,90 @@
-var $$ = require('nodeca-lib').Utilities;
+var nodeca = require('nodeca-lib'),
+    Store = nodeca.Settings.Store,
+    $$ = nodeca.Utilities;
+
+
+var Promise = require('simple-promise');
 
 
 module.exports = (function (app, callback) {
-  var store = {};
+  var UserGroupStore = function UserGroupStore() {
+    Store.call(this);
 
 
-  store.preload = function (env, callback) {
-    // grab user groups from user
-    if (undefined == env.groups && env.user && env.user.groups) {
-      env.groups = env.user.groups;
-    }
+    var STORE_KEY = '__usergroup_store__';
 
-    // env.grous was neither specified directly nor found in user
-    if (!Array.isArray(env.groups) || 0 == env.groups.length) {
-      callback(Error("Can't find list of groups"));
-      return;
-    }
 
-    // got list of groups ids - preload objects
-    if ('number' === typeof env.groups[0]) {
+    this.__.preloader = function (env, callback) {
+      // environment was preloaded before
+      if (env[STORE_KEY]) {
+        callback();
+        return;
+      }
+
+      // env.grous was neither specified directly nor found in user
+      if (!Array.isArray(env.groups) || 0 == env.groups.length) {
+        callback(Error("Can't find list of groups"));
+        return;
+      }
+
       app.model('usergroup')
       .where('_id').in(env.groups)
       .exec(function (err, groups) {
-        env.groups = groups;
+        env[STORE_KEY] = groups;
         callback(err);
       });
-      return;
-    }
-
-    // everything is ready
-    callback();
-  };
+    };
 
 
-  store.setter = function (data, env, callback) {
-    var UserGroup = app.model('usergroup'),
-        joint = new Promise.Joint(),
-        settings = {};
+    this.__.setter = function (data, env, callback) {
+      var UserGroup = app.model('usergroup'),
+          joint = new Promise.Joint(),
+          settings = {};
 
-    // prepare data to be set
-    $$.each(data, function (key, val) { settings['settings.' + key] = val; });
+      // prepare data to be set
+      $$.each(data, function (key, val) { settings['settings.' + key] = val; });
 
-    // call update for each group
-    env.groups.forEach(function (g) {
-      UserGroup.update({ _id: g._id }, settings, joint.promise().resolve);
-    });
-
-    // wait for all changes to be complete
-    joint.wait().done(function (err) {
-      var i;
-
-      // loop through promise results
-      for (i = 1; i < arguments.length; i++) {
-        // first argument (idx = 0) means error
-        if (arguments[i][0]) { // err
-          callback(arguments[i][0]);
-          return;
-        }
-      }
-
-      callback();
-    });
-  };
-
-
-  store.getter = function (keys, env, callback) {
-    var result = {};
-
-    $$.each(keys, function(key, val) {
-      results[key] = $$.map(env.groups, function (grp) {
-        return grp.settings[key];
+      // call update for each group
+      env[STORE_KEY].forEach(function (g) {
+        UserGroup.update({ _id: g._id }, settings, joint.promise().resolve);
       });
-    });
 
-    callback(null, result);
+      // wait for all changes to be complete
+      joint.wait().done(function (err) {
+        var i;
+
+        // loop through promise results
+        for (i = 1; i < arguments.length; i++) {
+          // first argument (idx = 0) means error
+          if (arguments[i][0]) { // err
+            callback(arguments[i][0]);
+            return;
+          }
+        }
+
+        callback();
+      });
+    };
+
+
+    this.__.getter = function (keys, env, callback) {
+      var result = {};
+
+      $$.each(keys, function(key, val) {
+        results[key] = $$.map(env[STORE_KEY], function (grp) {
+          return grp.settings[key];
+        });
+      });
+
+      callback(null, result);
+    };
   };
 
 
-  callback(null, store);
+  Store.adopts(UserGroupStore);
+
+
+  callback(null, new UserGroupStore());
 });
 
 
