ce8403c32e332f2bcfd6096173dd51ffe2658f86
Aleksey V Zapparov <ixti@member.fsf.org>
2012-02-05 16:20:03 +0100

    Add assets_server initialization

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-module.exports = new (require('nodeca-lib').Application)({
+"use strict";
+
+
+// stdlib
+var Fs = require('fs');
+
+
+// nodeca
+var NLib = require('nlib');
+
+
+// 3rd-party
+var StaticLulz = require('static-lulz');
+var FsTools = NLib.Vendor.FsTools;
+var Async = NLib.Vendor.Async;
+
+
+module.exports = NLib.Application.create({
   root: __dirname,
-  name: 'core'
+  name: 'nodeca.core',
+  bootstrap: function (nodeca, callback) {
+    // empty bootstrap... for now..
+    callback();
+  }
+});
+
+
+global.nodeca.hooks.init.after('bundles', function (next) {
+  nodeca.runtime.assets_server = new StaticLulz();
+
+  FsTools.walk(nodeca.runtime.assets_path, function (file, stats, next_file) {
+    // Fill in Static lulz with files and data
+    Async.waterfall([
+      Async.apply(Fs.readFile, file),
+      function (buffer, callback) {
+        var rel_path = file.replace(nodeca.runtime.assets_path, '');
+        nodeca.runtime.assets_server.add(rel_path, buffer);
+        callback();
+      }
+    ], next_file);
+  }, next);
+});
+
+
+global.nodeca.hooks.init.after('initialization', function (next) {
+  // start web server here
+  next();
 });
 
 
