386a1d6b0e35c24b41a431f025935fee4562b64f
Aleksey V Zapparov <ixti@member.fsf.org>
2012-02-10 13:47:44 +0100

    Update package.json

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ global.nodeca.hooks.init.after('bundles', function (next) {
 
 
 global.nodeca.hooks.init.after('initialization', function (next) {
-  // start web server here
+  nodeca.runtime.http_server = connect.createServer();
   next();
 });
 
