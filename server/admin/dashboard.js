2c66aeb04a628eedfb08dd943dd1ccef2cfc5bbb
Aleksey V Zapparov <ixti@member.fsf.org>
2012-03-06 17:21:24 +0100

    Add filters demo

--- server/admin/dashboard.js
+++ server/admin/dashboard.js
@@ -1,7 +1,26 @@
 'use strict';
 
 
+var nodeca = global.nodeca;
+
+
 module.exports = function (params, next) {
   this.data = {now: (new Date).toString()};
   next();
 };
+
+
+nodeca.filters.before('::admin', function (params, next) {
+  console.log('::admin fired');
+  next();
+});
+
+nodeca.filters.before('::admin.dashboard', function (params, next) {
+  console.log('::admin.dashboard fired');
+  next();
+});
+
+nodeca.filters.before('@', function (params, next) {
+  console.log('@ fired');
+  next();
+});
