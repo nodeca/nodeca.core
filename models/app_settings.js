d6a19e73c9707914d28b39279d89400063865934
Aleksey V Zapparov <ixti@member.fsf.org>
2012-03-13 11:32:49 +0100

    refactor dumb models

---
+++ models/app_settings.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+module.exports = new nodeca.runtime.mongoose.Schema({
+  app_name: { type: String },
+  settings: { type: nodeca.runtime.mongoose.Schema.Types.Mixed, default: {}}
+});
+
+
+
+module.exports.__init__ = function () {
+  return nodeca.runtime.mongoose.model('app-settings', module.exports);
+};
---
+++ models/user_group.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+module.exports = new nodeca.runtime.mongoose.Schema({
+  // shortcut name for the group
+  name:     { type: String },
+  // human readable title
+  title:    { type: String },
+  settings: { type: nodeca.runtime.mongoose.Schema.Types.Mixed, default: {}}
+});
+
+
+module.exports.__init__ = function () {
+  return nodeca.runtime.mongoose.model('user-group', module.exports);
+};
