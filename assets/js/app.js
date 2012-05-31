06ca60bc12c7ca15558aa8f84551a28b0c104ace
Aleksey V Zapparov <ixti@member.fsf.org>
2012-05-31 20:46:36 +0200

    Fix faye client-side

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
+/*global nodeca, $*/
+
+
+//= require jquery
+//= require underscore
 //= require nodeca
+//= require nodeca-io
+
+
+$(function () {
+  'use strict';
+
+  nodeca.io.init();
+
+  var $online = $('#online');
+  nodeca.io.subscribe('/stats/users_online', function (count) {
+    $online.text(count);
+  }).fail(function (err) {
+    nodeca.logger.error('Failed subscribe for stats updates: ' + err);
+  });
+});
