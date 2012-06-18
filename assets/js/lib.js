3c23e5e177687f7a5e5e6f61c134789b3486122b
Aleksey V Zapparov <ixti@member.fsf.org>
2012-06-18 02:06:31 +0200

    Added collapse events proxier

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -6,3 +6,15 @@
 //= require babelfish-runtime
 //= require pointer
 
+
+$(function () {
+  'use strict';
+
+  var events = $.map(['show', 'shown', 'hide', 'hidden'], function (evt) {
+    return evt + '.collapse.data-api';
+  }).join(' ');
+
+  $('body').on(events, '[data-notify]', function (event) {
+    $( $(this).data('notify') ).trigger(event.type);
+  });
+});
