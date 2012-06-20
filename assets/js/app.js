0484c897b641adf4e735719c6fb36795693a46ad
Aleksey V Zapparov <ixti@member.fsf.org>
2012-06-20 21:25:22 +0200

    Fix collapser

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,15 +5,3 @@
 
 
 nodeca.io.init();
-
-
-$(function () {
-  'use strict';
-
-  var $online = $('#online');
-  nodeca.io.subscribe('/stats/users_online', function (count) {
-    $online.text(count);
-  }).fail(function (err) {
-    nodeca.logger.error('Failed subscribe for stats updates: ' + err);
-  });
-});
