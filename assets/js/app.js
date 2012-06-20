e74affe225a689102191d5efd9559d07c9335949
Aleksey V Zapparov <ixti@member.fsf.org>
2012-06-20 21:42:24 +0200

    Add comment bout collapser fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,9 @@ nodeca.io.init();
 $(function () {
   'use strict';
   $('body').on('click.collapse.data-api', '[data-toggle=collapse]', function ( e ) {
+    // Bootstrap.Collapse calls e.preventDefault() only when there's no
+    // data-target attribute. We don't want URL to be changed, so we are
+    // forcing prevention of default behavior.
     e.preventDefault();
   });
 });
