56b9ab04caf0dab46a93acfe52d922cbd3eac049
Vitaly Puzrin <vitaly@rcdesign.ru>
2012-06-21 11:33:08 +0400

    style: added search & generic defs

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,10 +9,23 @@ nodeca.io.init();
 
 $(function () {
   'use strict';
+
+  // Bootstrap.Collapse calls e.preventDefault() only when there's no
+  // data-target attribute. We don't want URL to be changed, so we are
+  // forcing prevention of default behavior.
   $('body').on('click.collapse.data-api', '[data-toggle=collapse]', function ( e ) {
-    // Bootstrap.Collapse calls e.preventDefault() only when there's no
-    // data-target attribute. We don't want URL to be changed, so we are
-    // forcing prevention of default behavior.
     e.preventDefault();
   });
+
+  //
+  // Observe quicksearch focus to tweak icon style
+  //
+  $('.navbar-search .search-query').focus(
+    function(){
+        $(this).next('div').addClass('focused');
+    }).blur(
+    function(){
+        $(this).next('div').removeClass('focused');
+    });
+
 });
