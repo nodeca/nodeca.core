13f199813eb81586f0740905bb73e07d842c828f
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-09 01:07:31 +0200

    Fix loader

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -8,20 +8,17 @@ window.nodeca = {
   server: {},
   shared: {},
   runtime: {
-    load: function (assets, type) {
-      var list = [], asset;
+    load: function (assets) {
+      var i, l;
 
-      // get copy of array
-      assets = assets.slice();
-
-      while (assets.length) {
-        asset = assets.shift();
-        if (!type || type === asset.type) {
-          list.push(asset.link);
+      // simple version of Array#forEach
+      for (i = 0, l = assets.length ; i < l ; i++) {
+        if ('css' === assets[i].type && assets[i].media) {
+          yepnope.injectCss(assets[i].link, {media: assets[i].media});
+        } else {
+          yepnope(assets[i].link);
         }
       }
-
-      yepnope(list);
     }
   }
 };
