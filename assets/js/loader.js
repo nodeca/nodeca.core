7e3eb541538bc18c0c58aa62e77b3a5efddbe739
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-09 00:25:19 +0200

    Implement assets loader

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,2 +1,27 @@
 //= require modernizr.custom
 //= require yepnope/yepnope
+//= require_self
+
+
+window.nodeca = {
+  client: {},
+  server: {},
+  shared: {},
+  runtime: {
+    load: function (assets, type) {
+      var list = [], asset;
+
+      // get copy of array
+      assets = assets.slice();
+
+      while (assets.length) {
+        asset = assets.shift();
+        if (!type || type === asset.type) {
+          list.push(asset.link);
+        }
+      }
+
+      yepnope(list);
+    }
+  }
+};
