d956649395553054f18f8bc208e68af42dd6bb77
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-09 14:46:47 +0200

    Improve loader

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -3,22 +3,58 @@
 //= require_self
 
 
-window.nodeca = {
-  client: {},
-  server: {},
-  shared: {},
-  runtime: {
-    load: function (assets) {
-      var i, l;
+(function (nodeca) {
+  nodeca.client   = {};
+  nodeca.server   = {};
+  nodeca.shared   = {};
+  nodeca.runtime  = {};
 
-      // simple version of Array#forEach
-      for (i = 0, l = assets.length ; i < l ; i++) {
-        if ('css' === assets[i].type && assets[i].media) {
-          yepnope.injectCss(assets[i].link, {media: assets[i].media});
-        } else {
-          yepnope(assets[i].link);
-        }
-      }
+
+  // empty function
+  function noop() {}
+
+
+  function DefferedAssets(callback) {
+    this.callback = callback || noop;
+    this.waiting  = false;
+    this.queue    = 0;
+  }
+
+  DefferedAssets.prototype.listen = function () {
+    var self = this;
+
+    self.queue++;
+
+    return function () {
+      self.queue--;
+      self.done();
+    };
+  }
+
+  DefferedAssets.prototype.done = function () {
+    if (this.waiting && 0 === this.queue) {
+      this.callback();
     }
   }
-};
+
+  DefferedAssets.prototype.wait = function () {
+    this.waiting = true;
+    this.done();
+  };
+
+
+  nodeca.runtime.load = function (assets, callback) {
+    var i, l, d = new DefferedAssets(callback);
+
+    // simple version of Array#forEach
+    for (i = 0, l = assets.length ; i < l ; i++) {
+      if ('css' === assets[i].type && assets[i].media) {
+        yepnope.injectCss(assets[i].link, d.listen(), {media: assets[i].media});
+      } else {
+        yepnope({load: assets[i].link, complete: d.listen()});
+      }
+    }
+
+    d.wait();
+  };
+}(window.nodeca || window.nodeca = {}));
