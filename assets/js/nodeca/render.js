0e9ac59918c62ca0221af234e518599f4c672e00
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-10 15:20:57 +0200

    Implemented renderer partily

--- assets/js/nodeca/render.js
+++ assets/js/nodeca/render.js
@@ -16,7 +16,23 @@
   'use strict';
 
 
+  function find_fn(path) {
+    var val = nodeca.views, parts = path.split('.');
+
+    while (val && parts.length) {
+      val = val[parts.shift()];
+    }
+
+    return val && val[1] || function () {
+      alert("View " + path + " not found");
+    };
+  }
+
+
   nodeca.render = function (path, data) {
+    var html = find_fn(path)(data);
+
+    console.log(html, data);
     alert('Not implemented yet');
   };
 }());
