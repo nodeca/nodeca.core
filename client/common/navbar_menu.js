e728f1ae1a5b85704a3a658256b43272185a403b
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-17 20:22:38 +0200

    [data-api3-route] -> [data-api3-path]

--- client/common/navbar_menu.js
+++ client/common/navbar_menu.js
@@ -18,8 +18,8 @@
 
 
 /**
- *  client.common.navbar_menu.activate(route)
- *  - route (String): Server API path
+ *  client.common.navbar_menu.activate(apiPath)
+ *  - apiPath (String): Server API path
  *
  *  Sets `cssClass` for menu items of `route`.
  *
@@ -28,17 +28,17 @@
  *
  *      nodeca.client.common.navbar_menu.activate('forums.index');
  **/
-module.exports.activate = function (route) {
-  var ns = route.split('.').shift();
+module.exports.activate = function (apiPath) {
+  var ns = apiPath.split('.').shift();
 
   $('#navbar_menu')
-    .find('[data-api3-route]').removeClass('active').end()
+    .find('[data-api3-path]').removeClass('active').end()
     //
     // activate any element with same namespace as `route`:
     //
     //     route = 'foo.bar'
     //
-    // will highlight elements with `[data-api3-route="foo"]`.
+    // will highlight elements with `[data-api3-path="foo"]`.
     //
-    .find('[data-api3-route="' + ns + '"]').addClass('active');
+    .find('[data-api3-path="' + ns + '"]').addClass('active');
 };
