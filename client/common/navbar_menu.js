c58ee4ebbbc27704cfa2f58bf74e432aeddbe279
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-17 17:08:16 +0200

    Fix navbar menu item selector

--- client/common/navbar_menu.js
+++ client/common/navbar_menu.js
@@ -17,10 +17,6 @@
 /*global $, _*/
 
 
-// Prepare Underscore tempalte of data attribute selector
-var template = _.template('[data-api3-route="<%= route %>"]');
-
-
 /**
  *  client.common.navbar_menu.activate(route)
  *  - route (String): Server API path
@@ -33,17 +29,16 @@ var template = _.template('[data-api3-route="<%= route %>"]');
  *      nodeca.client.common.navbar_menu.activate('forums.index');
  **/
 module.exports.activate = function (route) {
-  var parts = route.split('.'), selectors = [];
+  var ns = route.split('.').shift();
 
-  // unset any active menu items
-  $('[data-api3-route]').removeClass('active');
-
-  // prepare list of selectors
-  while (parts.length) {
-    selectors.push(template({route: parts.join('.')}));
-    parts.pop();
-  }
-
-  // set new active menu items
-  $(selectors.join(',')).addClass('active');
+  $('#navbar_menu')
+    .find('[data-api3-route]').removeClass('active').end()
+    //
+    // activate any element with same namespace as `route`:
+    //
+    //     route = 'foo.bar'
+    //
+    // will highlight elements with `[data-api3-route="foo"]`.
+    //
+    .find('[data-api3-route="' + ns + '"]').addClass('active');
 };
