12403ec8adf25ab0aa7b937e8dc897ce19143791
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-17 17:23:58 +0200

    Refactor client app init

--- client/common/init.js
+++ client/common/init.js
@@ -24,62 +24,74 @@
  *      $(nodeca.client.common.init);
  **/
 module.exports = function () {
-  //
-  // Observe quicksearch focus to tweak icon style
-  //
-  $('.navbar-search .search-query')
-    .focus(function (){ $(this).next('div').addClass('focused'); })
-    .blur(function (){ $(this).next('div').removeClass('focused'); });
+  nodeca.io.init();
+
+  $(function () {
+    // Bootstrap.Collapse calls e.preventDefault() only when there's no
+    // data-target attribute. We don't want URL to be changed, so we are
+    // forcing prevention of default behavior.
+    $('body').on('click.collapse.data-api', '[data-toggle=collapse]', function ( e ) {
+      e.preventDefault();
+    });
 
 
-  //
-  // Observe clicks on anchors
-  //
+    //
+    // Observe quicksearch focus to tweak icon style
+    //
+    $('.navbar-search .search-query')
+      .focus(function (){ $(this).next('div').addClass('focused'); })
+      .blur(function (){ $(this).next('div').removeClass('focused'); });
 
-  var History = window.History; // History.js
 
-  function call_api3(href, match) {
-    nodeca.io.apiTree(match.meta, match.params, function (err, msg) {
-      // TODO: Properly handle `err` and (?) `msg.error`
-      if (err) {
-        nodeca.logger.error('Failed apiTree call', err);
-        return;
-      }
+    //
+    // Observe clicks on anchors
+    //
 
-      try {
-        nodeca.render(msg.view || match.meta, msg.layout, msg.data);
-      } catch (err) {
-        if ('NODECA_PLACEHOLDER_NOT_FOUND' === err) {
-          window.location = href;
+    var History = window.History; // History.js
+
+    function call_api3(href, match) {
+      nodeca.io.apiTree(match.meta, match.params, function (err, msg) {
+        // TODO: Properly handle `err` and (?) `msg.error`
+        if (err) {
+          nodeca.logger.error('Failed apiTree call', err);
           return;
         }
 
-        // FIXME: redirect on error? or at least propose user to click
-        //        a link to reload to the requested page
-        nodeca.logger.error('Failed render view <' + (msg.view || match.meta) +
-                            '> with layout <' + msg.layout + '>', err);
-        return;
-      }
+        try {
+          nodeca.render(msg.view || match.meta, msg.layout, msg.data);
+        } catch (err) {
+          if ('NODECA_PLACEHOLDER_NOT_FOUND' === err) {
+            window.location = href;
+            return;
+          }
 
-      History.pushState(match, msg.data.head.title, href);
-      nodeca.client.common.navbar_menu.activate(msg.data.head.route || match.meta);
-    });
-  }
+          // FIXME: redirect on error? or at least propose user to click
+          //        a link to reload to the requested page
+          nodeca.logger.error('Failed render view <' + (msg.view || match.meta) +
+                              '> with layout <' + msg.layout + '>', err);
+          return;
+        }
 
-  if (History.enabled) {
-    $('body').on('click', 'a', function (event) {
-      var href  = $(this).attr('href'),
-          match = href && nodeca.runtime.router.match(href.split('#')[0]);
+        History.pushState(match, msg.data.head.title, href);
+        nodeca.client.common.navbar_menu.activate(msg.data.head.route || match.meta);
+      });
+    }
 
-      if (match) {
-        call_api3(href, match);
-        event.preventDefault();
-        return false;
-      }
-    });
+    if (History.enabled) {
+      $('body').on('click', 'a', function (event) {
+        var href  = $(this).attr('href'),
+            match = href && nodeca.runtime.router.match(href.split('#')[0]);
 
-    History.Adapter.bind(window, 'statechange', function (event) {
-      console.log(History.getState(), event);
-    });
-  }
+        if (match) {
+          call_api3(href, match);
+          event.preventDefault();
+          return false;
+        }
+      });
+
+      History.Adapter.bind(window, 'statechange', function (event) {
+        console.log(History.getState(), event);
+      });
+    }
+  });
 };
