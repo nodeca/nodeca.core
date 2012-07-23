3b7ef164b935b7865ef38b06e60b71b987366904
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-23 17:35:39 +0200

    Add inject_tree method

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -7,10 +7,62 @@
 /*global yepnope*/
 
 
-var load_assets = window.load_assets = (function () {
+(function () {
   'use strict';
 
 
+  var toString  = Object.prototype.toString,
+      hasOwn    = Object.prototype.hasOwnProperty;
+
+
+  // check if `obj` is a function
+  function isFunction(obj) {
+    return '[object Function]' === toString(obj);
+  }
+
+
+  // check if `obj` is array
+  function isArray(obj) {
+    return '[object Array]' === toString(obj);
+  }
+
+
+  // check if `obj` is a plain object: not a null, underfined, function, array
+  // or instance of something else.
+  function isPlainObject(obj) {
+    // Must be an Object.
+    // Because of IE, we also have to check the presence of the constructor property.
+    if (!obj || '[object Object]' !== toString(obj)) {
+      return false;
+    }
+
+    try {
+      // Not own constructor property must be Object
+      if (obj.constructor &&
+        !hasOwn.call(obj, "constructor") &&
+        !hasOwn.call(obj.constructor.prototype, "isPrototypeOf")) {
+        return false;
+      }
+    } catch ( e ) {
+      // IE8,9 Will throw exceptions on certain host objects #9897
+      return false;
+    }
+
+    // Own properties are enumerated firstly, so to speed up,
+    // if last one is own, then all properties are own.
+
+    /*jshint noempty:false*/
+    for (var key in obj) {}
+
+    return key === undefined || hasOwn.call(obj, key);
+  }
+
+
+  // returns array of `prop` values from `obj`
+  //
+  //    collect([{foo: 1}, {foo: 2}], 'foo');
+  //    // -> [1, 2]
+  //
   function collect(obj, prop) {
     var out = [], i;
 
@@ -23,7 +75,20 @@ var load_assets = window.load_assets = (function () {
     return out;
   }
 
+
+  // Simple `each` iterator
+  function each(obj, iter) {
+    for (var k in obj) {
+      if (obj.hasOwnProperty(k)) {
+        iter(obj[k], k);
+      }
+    }
+  }
+
+
+  // cached non-operable function
   function noop() {}
+
 
   function load_assets(assets, callback) {
     callback = callback || noop;
@@ -36,5 +101,31 @@ var load_assets = window.load_assets = (function () {
     yepnope({load: collect(assets, 'link'), complete: callback});
   }
 
-  return load_assets;
+
+  function inject_tree(tree, branch) {
+    // make sure tree is a plain object or a function
+    tree = (isPlainObject(tree) || isFunction(tree)) && tree || {};
+
+    each(branch || {}, function (val, key) {
+      if (isPlainObject(val)) {
+        tree[key] = inject_tree(tree[key], val);
+        return;
+      }
+
+      if (isFunction(val)) {
+        tree[key] = inject_tree(val, inject_tree(tree[key], val));
+        return;
+      }
+
+      // plain value - do not try to merge - just override
+      tree[key] = val;
+    });
+
+    // Return the modified object
+    return tree;
+  }
+
+
+  window.load_assets = load_assets;
+  window.inject_tree = inject_tree;
 }());
