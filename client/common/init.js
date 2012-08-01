7085e903bfde5fc637428d5cd36bb7c697d624ad
Aleksey V Zapparov <ixti@member.fsf.org>
2012-08-01 12:49:45 +0200

    Add .controlbar-fixed like in Bootstrap

--- client/common/init.js
+++ client/common/init.js
@@ -45,6 +45,44 @@ module.exports = function () {
   });
 
 
+  $(function () {
+    // fix sub nav on scroll
+    var $win = $(window),
+        $nav = $('.controlbar'),
+        navTop = $('.controlbar').length && $('.controlbar').offset().top,
+        isFixed = 0;
+
+    function processScroll() {
+      var i, scrollTop = $win.scrollTop();
+
+      if (scrollTop >= navTop && !isFixed) {
+        isFixed = 1;
+        $nav.addClass('controlbar-fixed');
+        return;
+      }
+
+      if (scrollTop <= navTop && isFixed) {
+        isFixed = 0;
+        $nav.removeClass('controlbar-fixed');
+        return;
+      }
+    }
+
+    processScroll();
+
+    $win.on('scroll', processScroll);
+
+    // hack sad times - holdover until rewrite for 2.1
+    $nav.on('click', function () {
+      if (!isFixed) {
+        setTimeout(function () {
+          $win.scrollTop($win.scrollTop() - 47);
+        }, 10);
+      }
+    });
+  });
+
+
   // assign action links handlers
   nodeca.client.common.init.action_links();
 
