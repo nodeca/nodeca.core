7c247e64f04f6dc407856be8678d5bdd6b9b58cd
Aleksey V Zapparov <ixti@member.fsf.org>
2012-09-06 17:01:49 +0200

    Small fixes after review

--- server/common/locale.js
+++ server/common/locale.js
@@ -1,19 +1,16 @@
 'use strict';
 
+
 nodeca.validate({
   locale: { type: 'string', required: true }
 });
 
+
 module.exports = function (params, next) {
-  if (!params.locale) {
+  if (-1 === nodeca.config.locales.enabled.indexOf(params.locale)) {
     params.locale = nodeca.config.locales['default'];
   }
 
-  if (-1 < nodeca.config.locales.enabled.indexOf(params.locale)) {
-    this.session.locale = params.locale;
-    next();
-    return;
-  }
-
-  next({ statusCode: 404, message: 'Unknown locale ' + params.locale });
+  this.session.locale = params.locale;
+  next();
 };
