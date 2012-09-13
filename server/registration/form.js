8c2e9b7cf786de822ef9739560f169745d6f48cd
Eugene Shkuropat <e.shkuropat@gmail.com>
2012-09-13 18:21:24 +0400

    Fixed registration form

--- server/registration/form.js
+++ server/registration/form.js
@@ -2,24 +2,8 @@
 
 /*global nodeca, _*/
 
-// form params description
-//
-var form_params_schema = {
-  // forum id
-  email: {
-    type: "email",
-    minimum: 1,
-    required: true
-  },
-  pass: {
-    type: "password",
-    minimum: 8,
-    required: true
-  }
-};
 
-
-// fields can be fwtched from oauth provider
+// fields can be fetched from session, if oauth provider give them
 var profile_in_fields = [
   'email',
   'first_name',
@@ -31,13 +15,15 @@ var profile_in_fields = [
 nodeca.validate();
 
 
-// bind provider to account
-//
+/**
+ * registration.form(params, callback) -> Void
+ *
+ * Render registration form
+ **/
 module.exports = function (params, next) {
   this.response.data.predefined = {};
-  this.response.data.form_fields = form_params_schema;
   if (this.session.auth_data) {
-    // set predefined data from proveder
+    // set predefined data from session
     this.response.data.predefined = _.pick(this.session.auth_data, _.keys(profile_in_fields));
   }
   next();
