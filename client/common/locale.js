83f59c8d3a506e6d9d149f8530c655d0b1490795
Aleksey V Zapparov <ixti@member.fsf.org>
2012-09-17 22:31:20 +0200

    Bind languages menu to change locale

--- client/common/locale.js
+++ client/common/locale.js
@@ -1,10 +1,18 @@
 'use strict';
 
-module.exports = function (locale) {
+
+/*global window, nodeca*/
+
+
+module.exports = function ($el, event) {
+  var locale = $el.data('locale');
+
   nodeca.server.common.locale({ locale: locale }, function (err) {
     if (!err) {
       nodeca.runtime.locale = locale;
       window.location.reload();
     }
   });
+
+  return false;
 };
