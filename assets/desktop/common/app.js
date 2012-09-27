0195932db335771a2c5a46b8703485ae16671577
Aleksey V Zapparov <ixti@member.fsf.org>
2012-09-27 23:17:56 +0200

    Revert "Fix noty inclusion"
    This reverts commit 7a5c913331b8306e7a1fb589fe73471c8c411590.

--- assets/desktop/common/app.js
+++ assets/desktop/common/app.js
@@ -1,3 +1,85 @@
-//= require noty/jquery.noty
-//= require ./noty/layouts
-//= require ./noty/theme
+//= require noty
+
+
+/*global window, jQuery*/
+
+
+;(function($) {
+  'use strict';
+
+
+  $.noty.layouts.progress = {
+    name: 'progress',
+    options: {}, // overrides options
+    container: {
+      object: '<ul id="noty_topCenter_layout_container" />',
+      selector: 'ul#noty_topCenter_layout_container',
+      style: function() {
+        $(this).css({
+          top: 20,
+          left: 0,
+          position: 'fixed',
+          width: '310px',
+          height: 'auto',
+          margin: 0,
+          padding: 0,
+          listStyleType: 'none',
+          zIndex: 10000000
+        });
+
+        $(this).css({
+          left: ($(window).width() - $(this).outerWidth()) / 2 + 'px'
+        });
+      }
+    },
+    parent: {
+      object: '<li />',
+      selector: 'li',
+      css: {}
+    },
+    css: {
+      display: 'none',
+      width: '310px'
+    },
+    addClass: ''
+  };
+
+
+  $.noty.layouts.notification = {
+    name: 'notification',
+    options: {}, // overrides options
+    container: {
+      object: '<ul id="noty_topRight_layout_container" />',
+      selector: 'ul#noty_topRight_layout_container',
+      style: function() {
+        $(this).css({
+          top: 20,
+          right: 20,
+          position: 'fixed',
+          width: '310px',
+          height: 'auto',
+          margin: 0,
+          padding: 0,
+          listStyleType: 'none',
+          zIndex: 10000000
+        });
+
+        if (window.innerWidth < 600) {
+          $(this).css({
+            right: 5
+          });
+        }
+      }
+    },
+    parent: {
+      object: '<li />',
+      selector: 'li',
+      css: {}
+    },
+    css: {
+      display: 'none',
+      width: '310px'
+    },
+    addClass: ''
+  };
+})(jQuery);
