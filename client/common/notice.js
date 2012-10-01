1c3b79273d70fbb319aa3a08fbaeab26f3d9e653
Aleksey V Zapparov <ixti@member.fsf.org>
2012-10-01 22:50:18 +0200

    Cleanup notification functions

--- client/common/notice.js
+++ client/common/notice.js
@@ -4,39 +4,15 @@
 /*global $, nodeca*/
 
 
-var stacks = {};
-
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-stacks.info = stacks.success = stacks.error = (function () {
-  var $container = $([]);
-
-  return function (message, options) {
-    var notice;
-
-    if (!$container.length) {
-      $container = $('<div class="notifications top-right" />').appendTo('body');
-    }
-
-    notice = $container.notify({
-      type:       options.type,
-      closable:   ( 'undefined' === typeof options.closable ) ? true : !!options.closable,
-      fadeOut:    { enabled: !!options.autoclose, delay: +options.autoclose || 3000 },
-      message:    { html: message }
-    });
-
-    notice.show();
-    return notice;
-  };
-}());
+var $container = $([]);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 
 
 module.exports = function (type, message, options) {
+  var notice;
+
   if ('string' !== typeof message) {
     options = message;
     message = type;
@@ -45,7 +21,18 @@ module.exports = function (type, message, options) {
 
   options = options || {};
   message = message || '';
-  type    = options.type = !!stacks[type] ? type : 'error';
 
-  return stacks[type](message, options);
+  if (!$container.length) {
+    $container = $('<div class="notifications top-right" />').appendTo('body');
+  }
+
+  notice = $container.notify({
+    type:       type,
+    closable:   ( 'undefined' === typeof options.closable ) ? true : !!options.closable,
+    fadeOut:    { enabled: !!options.autoclose, delay: +options.autoclose || 3000 },
+    message:    { html: message }
+  });
+
+  notice.show();
+  return notice;
 };
