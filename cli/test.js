308fc48685f489510a639a12aba125000e7d2e9c
Aleksey V Zapparov <ixti@member.fsf.org>
2012-10-14 03:30:05 +0200

    Refactor tests clie runner

--- cli/test.js
+++ cli/test.js
@@ -1,12 +1,26 @@
 "use strict";
 
 
-/*global nodeca*/
+/*global nodeca, _*/
 
-// nodeca
-var NLib = require('nlib');
 
-var Async = NLib.Vendor.Async;
+// stdlib
+var fs        = require('fs');
+var path      = require('path');
+var resolve   = path.resolve;
+var exists    = fs.existsSync || path.existsSync;
+var join      = path.join;
+var basename  = path.basename;
+
+
+// 3rd-party
+var NLib      = require('nlib');
+var Async     = require('nlib').Vendor.Async;
+var Mocha     = require('mocha');
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 module.exports.parserParameters = {
   version: nodeca.runtime.version,
@@ -15,7 +29,59 @@ module.exports.parserParameters = {
   description: 'Run all tests of enabled apps'
 };
 
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+require('should');
 process.env.NODECA_ENV = 'test';
+
+
+// Lookup file names at the given `path`.
+//
+function lookupFiles(path, recursive) {
+  var files = [], stat;
+
+  if (!exists(path)) {
+    path += '.js';
+  }
+
+  try {
+    stat = fs.statSync(path);
+  } catch (err) {
+    return files;
+  }
+
+  if (stat.isFile()) {
+    return path;
+  }
+
+  fs.readdirSync(path).forEach(function(file){
+    file = join(path, file);
+
+    var stat = fs.statSync(file);
+
+    if (stat.isDirectory()) {
+      if (recursive) {
+        files = files.concat(lookupFiles(file, recursive));
+      }
+
+      return;
+    }
+
+    if (!stat.isFile() || !(/\.js$/).test(file) || '.' === basename(file)[0]) {
+      return;
+    }
+
+    files.push(file);
+  });
+
+  return files;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 module.exports.run = function (args, callback) {
   Async.series([
@@ -35,6 +101,23 @@ module.exports.run = function (args, callback) {
     NLib.InitStages.initTranslations,
 
     require('../lib/init/assets'),
-    require('../lib/init/tests')
+
+    function (next) {
+      var mocha = new Mocha(), files = [];
+
+
+      mocha.reporter('spec');
+      mocha.ui('bdd');
+
+      _.each(nodeca.runtime.apps, function (app) {
+        files = files.concat(lookupFiles(app.root + '/test', true));
+      });
+
+      mocha.files = files.map(function (path) {
+        return resolve(path);
+      });
+
+      mocha.run(next);
+    }
   ], callback);
 };
