c1d4cd198506fb541808bae3f11fbef4978d3fe7
Aleksey V Zapparov <ixti@member.fsf.org>
2013-01-07 15:45:07 +0100

    Small fix

--- index.js
+++ index.js
@@ -4,9 +4,4 @@ exports.root = __dirname;
 exports.name = 'nodeca.core';
 exports.init = function () {
   require('./lib/filters');
-
-  N.hooks.init.after('application', function (next) {
-    console.dir(N.views);
-    next();
-  });
 };
