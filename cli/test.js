0993db8e01697b7cd511651bb305a4fc6adb67df
Aleksey V Zapparov <ixti@member.fsf.org>
2013-01-11 17:29:34 +0100

    Fix nodeca test CLI script

--- cli/test.js
+++ cli/test.js
@@ -7,17 +7,20 @@
 // stdlib
 var fs        = require('fs');
 var path      = require('path');
-var resolve   = path.resolve;
-var exists    = fs.existsSync || path.existsSync;
-var join      = path.join;
-var basename  = path.basename;
 
 
 // 3rd-party
 var _         = underscore;
 var chai      = require('chai');
 var async     = require('async');
-var mocha     = require('mocha');
+var Mocha     = require('mocha');
+var fstools   = require('fs-tools');
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+chai.should();
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -47,94 +50,37 @@ module.exports.commandLineArguments = [
 ////////////////////////////////////////////////////////////////////////////////
 
 
-chai.should();
-
-
-// Lookup file names at the given `path`.
-//
-function lookupFiles(path, recursive) {
-  var files = [], stat;
-
-  if (!exists(path)) {
-    path += '.js';
-  }
-
-  try {
-    stat = fs.statSync(path);
-  } catch (err) {
-    return files;
-  }
-
-  if (stat.isFile()) {
-    return path;
-  }
-
-  fs.readdirSync(path).forEach(function (file) {
-    file = join(path, file);
-
-    var stat = fs.statSync(file);
-
-    if (stat.isDirectory()) {
-      if (recursive) {
-        files = files.concat(lookupFiles(file, recursive));
-      }
-
-      return;
-    }
-
-    if (!stat.isFile() || !(/\.js$/).test(file) || '.' === basename(file)[0]) {
-      return;
-    }
-
-    files.push(file);
-  });
-
-  return files;
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-
-
 module.exports.run = function (args, callback) {
   if (!process.env.NODECA_ENV) {
     callback("You must provide NODECA_ENV in order to run nodeca test");
     return;
   }
 
-  Async.series([
-    require('../lib/init/redis'),
-    require('../lib/init/mongoose'),
-
-    NLib.InitStages.loadModels,
-
-    require('../lib/init/migrations_check'),
-
-    NLib.InitStages.loadServerApiSubtree,
-    NLib.InitStages.loadSharedApiSubtree,
-    NLib.InitStages.loadClientApiSubtree,
-    NLib.InitStages.loadSettings,
-    NLib.InitStages.initRouter,
-
-    NLib.InitStages.initTranslations,
-
-    require('../lib/init/assets'),
+  async.series([
+    require('../lib/system/init/redis'),
+    require('../lib/system/init/mongoose'),
+    require('../lib/system/init/models'),
+    require('../lib/system/init/stores'),
+    require('../lib/system/init/check_migrations'),
+    require('../lib/system/init/bundle'),
+    // router needs to go after bundle,
+    // as server tree got polluted in bundle
+    require('../lib/system/init/router'),
 
     function (next) {
-      var mocha = new Mocha(), files = [];
-
+      var mocha = new Mocha();
 
       mocha.reporter('spec');
       mocha.ui('bdd');
 
-      _.each(nodeca.runtime.apps, function (app) {
+      _.each(N.runtime.apps, function (app) {
         if (!args.app || args.app === app.name) {
-          files = files.concat(lookupFiles(app.root + '/test', true));
+          fstools.walkSync(app.root + '/test', function (file) {
+            if ((/\.js$/).test(file) && '.' !== path.basename(file)[0]) {
+              mocha.files.push(file);
+            }
+          });
         }
-      });
-
-      mocha.files = files.map(function (path) {
-        return resolve(path);
       });
 
       mocha.run(next);
