7eaec69eb5dfd4a48b43a35fe88a86c730ea4a8d
Dervus Grim <dervus@lavabit.com>
2013-02-26 20:39:12 +0100

    Use Lodash from NPM only.

--- assets/javascripts/lib.js
+++ assets/javascripts/lib.js
@@ -1,8 +1,6 @@
 //= require vendor/jquery/jquery
 //= require vendor/powertip/jquery.powertip
 
-//= require vendor/lodash
-
 //= require vendor/bootstrap/bootstrap
 //= require babelfish-runtime
 //= require pointer
