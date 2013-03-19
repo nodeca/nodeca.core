3a69f4d9ecc38d1c1f2130934fa2ad1763b377e8
Dervus Grim <dervus@lavabit.com>
2013-03-19 15:31:31 +0100

    Use `pointer` and `babelfish` via regular `require` on the client-side.

--- assets/javascripts/lib.js
+++ assets/javascripts/lib.js
@@ -2,8 +2,6 @@
 //= require vendor/powertip/jquery.powertip
 
 //= require vendor/bootstrap/bootstrap
-//= require babelfish-runtime
-//= require pointer
 //= require vendor/history/history.adapter.jquery
 //= require vendor/history/history
 //= require vendor/jade/runtime
