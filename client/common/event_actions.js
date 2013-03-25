9164fa6f05c9db6b8d1913da02ebf9cac49e3885
Dervus Grim <dervus@lavabit.com>
2013-03-25 19:16:33 +0100

    Port the client-side global event actions handler.

---
+++ client/common/event_actions.js
@@ -0,0 +1,37 @@
+/**
+ *  Assigns handlers/listeners for `[data-action]` links.
+ *
+ *  Actions associated with a link will be invoked via Wire with the jQuery
+ *  event object as an argument.
+ **/
+
+
+'use strict';
+
+
+/*global NodecaLoader, N, window*/
+
+
+var $ = window.jQuery;
+
+
+$(function () {
+  ['click', 'submit', 'input'].forEach(function (action) {
+    var eventName = action + '.nodeca.data-api'
+      , attribute = '[data-on-' + action + ']';
+
+    $('body').on(eventName, attribute, function (event) {
+      var apiPath = $(this).data('on-' + action);
+
+      NodecaLoader.loadAssets(apiPath.split('.').shift(), function () {
+        if (N.wire.has(apiPath)) {
+          N.wire.emit(apiPath, event);
+        } else {
+          N.logger.error('Unknown client Wire handler: %s', apiPath);
+        }
+      });
+
+      return false;
+    });
+  });
+});
