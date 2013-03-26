6d0c5708ed39ee8f37f9a0f6d6dc402b8a50cc8e
Dervus Grim <dervus@lavabit.com>
2013-03-26 14:10:09 +0100

    Copy some of client scripts from `common` package to `admin`.

---
+++ client/admin/io_errors.js
@@ -0,0 +1,25 @@
+// automate serious errors notificating
+//
+
+
+'use strict';
+
+
+/*global N, t*/
+
+
+var notify = require('./_notify');
+
+
+N.wire.on('io.error', function (err) {
+  if (N.io.INVALID_CSRF_TOKEN === err.code) {
+    N.runtime.csrf = err.data.token;
+    notify(t('invalid_csrf_token'));
+
+  } else if (N.io.APP_ERROR === err.code) {
+    notify(t('application_fuckup'));
+
+  } else if (N.io.ECOMMUNICATION === err.code) {
+    notify(t('communication_timeout'));
+  }
+});
