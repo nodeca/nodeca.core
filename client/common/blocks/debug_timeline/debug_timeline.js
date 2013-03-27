d986dea9834ee737fcbf7f6a3c0e28b155bc439b
Dervus Grim <dervus@lavabit.com>
2013-03-27 19:41:09 +0100

    Complete client script of `debug_timeline` block.

--- client/common/blocks/debug_timeline/debug_timeline.js
+++ client/common/blocks/debug_timeline/debug_timeline.js
@@ -1,37 +1,15 @@
 /**
- *  injectStats(data) -> Void
- *  - data (Object): Locals data for the renderer
- *
- *  Renders and injects debug stats data if needed.
+ *  Renders and injects debug stats on successful RPC requests.
  **/
 
 
 'use strict';
 
 
-var $puncher_stats = null;
-
-
-module.exports = function injectStats(data) {
-  var html;
-
-  // try to find puncher stats first time
-  if (!$puncher_stats) {
-    $puncher_stats = $('#debug_timeline');
-  }
-
-  if (!$puncher_stats.length) {
-    // server didn't injected puncher stats so we don't
-    return;
-  }
-
-  html = N.runtime.render('common.blocks.debug_timeline', {
-    stats: data.blocks.puncher_stats
-  });
-
-  // replace HTML with new stats
-  $puncher_stats.replaceWith(html);
-
-  // recache jQuery element
-  $puncher_stats = $('#debug_timeline');
-};
+N.wire.on('io.success', function debug_timeline(response) {
+  $('#debug_timeline').replaceWith(
+    N.runtime.render(module.apiPath, {
+      stats: response.data.blocks.puncher_stats
+    })
+  );
+});
