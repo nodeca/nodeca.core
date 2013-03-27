f234e2fe1d6f0a43329dfa9a59dac6afa7d511fc
Dervus Grim <dervus@lavabit.com>
2013-03-27 13:26:47 +0100

    Rename `data-api3-path` attribute to `data-api-path`.

--- client/common/blocks/navbar_menu/navbar_menu.js
+++ client/common/blocks/navbar_menu/navbar_menu.js
@@ -16,13 +16,13 @@ N.wire.on('navigate.exit', function navbar_menu_change_active(target) {
   var ns = target.apiPath.split('.').shift();
 
   $('#navbar_menu')
-    .find('[data-api3-path]').removeClass('active').end()
+    .find('[data-api-path]').removeClass('active').end()
     //
     // activate any element with same namespace as `route`:
     //
     //     route = 'foo.bar'
     //
-    // will highlight elements with `[data-api3-path="foo"]`.
+    // will highlight elements with `[data-api-path="foo"]`.
     //
-    .find('[data-api3-path="' + ns + '"]').addClass('active');
+    .find('[data-api-path="' + ns + '"]').addClass('active');
 });
