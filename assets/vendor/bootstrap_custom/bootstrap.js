395cadea78bb32029dd39e14639404c9246a9e79
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-03-29 16:59:46 +0400

    switched bootstrap management to bower and updated to 2.3.1

---
+++ assets/vendor/bootstrap_custom/bootstrap.js
@@ -0,0 +1,16 @@
+//= //require ../bootstrap/js/bootstrap-affix
+//= require ../bootstrap/js/bootstrap-alert
+//= require ../bootstrap/js/bootstrap-button
+//= //require ../bootstrap/js/bootstrap-carousel
+//= require ../bootstrap/js/bootstrap-collapse
+//= require ../bootstrap/js/bootstrap-dropdown
+//= require ../bootstrap/js/bootstrap-modal
+//= //require ../bootstrap/js/bootstrap-scrollspy
+//= //require ../bootstrap/js/bootstrap-tab
+//= require ../bootstrap/js/bootstrap-tooltip
+//= require ../bootstrap/js/bootstrap-transition
+//= require ../bootstrap/js/bootstrap-typeahead
+
+// Popover must go AFTER tooltip
+//= require ../bootstrap/js/bootstrap-popover
+
