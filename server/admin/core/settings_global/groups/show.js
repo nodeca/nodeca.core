07984dfafc632764887615c6fb7c87755a592940
Dervus Grim <dervus@lavabit.com>
2013-04-19 20:58:08 +0200

    Add setting counts on global setting groups page.

--- server/admin/core/settings_global/groups/show.js
+++ server/admin/core/settings_global/groups/show.js
@@ -8,8 +8,9 @@ module.exports = function (N, apiPath) {
   N.validate(apiPath, {});
 
   N.wire.on(apiPath, function global_settings_index(env) {
-    var tabs   = env.response.data.tabs   = []
-      , groups = env.response.data.groups = {};
+    var tabs     = env.response.data.tabs   = []
+      , groups   = env.response.data.groups = {}
+      , settings = N.config.setting_schemas['global'];
 
     // Collect tabs, i.e. groups without `parent`.
     _.forEach(N.config.setting_groups, function (tabConfig, tabName) {
@@ -20,7 +21,10 @@ module.exports = function (N, apiPath) {
         // Collect groups per tab.
         _.forEach(N.config.setting_groups, function (groupConfig, groupName) {
           if (groupConfig && groupConfig.parent === tabName) {
-            groups[tabName].push(groupName);
+            groups[tabName].push({
+              name: groupName
+            , settingsCount: _.where(settings, { group_key: groupName }).length
+            });
           }
         });
       }
