2f7dcfbce78f7e2a2d9cd6df287c7e10fbba3be3
Dervus Grim <dervus@lavabit.com>
2013-05-17 16:10:21 +0200

    Implement navbar's active tab switch on RPC navigation.

---
+++ client/admin/core/layout/layout.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+var _ = require('lodash');
+
+
+//
+// Update "active" tab of the navbar_menu when moving to another page.
+//
+N.wire.on('navigate.done', function navbar_menu_change_active(target) {
+  var targetPath = target.apiPath.split('.'), tabs, active;
+
+  tabs = $('#navbar_menu').find('[data-api-path]');
+  tabs.removeClass('active');
+
+  // Select the most specific tab - with the longest API path match.
+  active = _.max(tabs, function (tab) {
+    var tabPath = $(tab).data('apiPath').split('.')
+      , index   = -1
+      , length  = Math.min(tabPath.length, targetPath.length);
+
+    do { index += 1; }
+    while (index < length && tabPath[index] === targetPath[index]);
+
+    return index;
+  });
+
+  $(active).addClass('active');
+});
