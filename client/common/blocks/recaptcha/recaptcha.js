3a33dd6225f8550426f42c4d9aafbc59e719e4bf
Dervus Grim <dervus@lavabit.com>
2013-05-28 23:50:57 +0200

    Improve internal ReCaptcha client module.
    Provide a Wire interface to dynamically create and update ReCaptcha.
    It's needed for reload ReCaptcha without reloading whole #content.

---
+++ client/common/blocks/recaptcha/recaptcha.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+/*global yepnope*/
+
+
+N.wire.on(module.apiPath + '.create', function recaptcha_create(__, callback) {
+  yepnope({
+    load: 'http://www.google.com/recaptcha/api/js/recaptcha_ajax.js',
+    complete: function () {
+      if (!window.Recaptcha) {
+        callback(new Error('Cannot load ReCaptcha script.'));
+        return;
+      }
+
+      window.Recaptcha.create(N.runtime.recaptcha.public_key, 'recaptcha_div', {
+        theme: 'custom'
+      });
+      callback();
+    }
+  });
+});
+
+
+N.wire.on(module.apiPath + '.update', function recaptcha_update() {
+  if (!window.Recaptcha) {
+    return new Error('Cannot update ReCaptcha since it is not loaded.');
+  }
+
+  window.Recaptcha.reload();
+  window.Recaptcha.focus_response_field();
+});
