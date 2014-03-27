1256bb128f947296c6939582831e4e2f066e453a
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-03-28 00:25:22 +0400

    fixed recaptcha load

--- client/common/blocks/recaptcha/recaptcha.js
+++ client/common/blocks/recaptcha/recaptcha.js
@@ -1,24 +1,27 @@
 'use strict';
 
 
-/*global yepnope*/
-
-
 N.wire.on(module.apiPath + '.create', function recaptcha_create(__, callback) {
-  yepnope({
-    load: '//www.google.com/recaptcha/api/js/recaptcha_ajax.js',
-    complete: function () {
-      if (!window.Recaptcha) {
-        callback(new Error('Cannot load ReCaptcha script.'));
-        return;
-      }
 
-      window.Recaptcha.create(N.runtime.recaptcha.public_key, 'recaptcha_div', {
-        theme: 'custom'
-      });
+  var rc_url = '//www.google.com/recaptcha/api/js/recaptcha_ajax.js';
+  var rc_key = N.runtime.recaptcha.public_key;
+  var rc_id  = 'recaptcha_div';
+  var rc_options = { theme: 'custom' };
+
+  if (window.ReCaptcha) {
+    window.Recaptcha.create(rc_key, rc_id, rc_options);
+    callback();
+    return;
+  }
+
+  $.getScript(rc_url)
+    .done(function () {
+      window.Recaptcha.create(rc_key, rc_id, rc_options);
       callback();
-    }
-  });
+    })
+    .fail(function () {
+      callback(new Error('Cannot load ReCaptcha script.'));
+    });
 });
 
 
