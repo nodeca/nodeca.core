c9943f7cf32aa87658d02adbad085b6494fd6ecc
Kirill Efimov <kirill89@gmail.com>
2014-06-09 20:47:12 +0400

    Common menu logic moved to client side

--- client/common/blocks/nav_vert/nav_vert.js
+++ client/common/blocks/nav_vert/nav_vert.js
@@ -67,3 +67,30 @@ N.wire.on('common.blocks.nav_vert.scroll', function () {
     }
   });
 });
+
+
+// Update "active" tab of the navbar_menu when moving to another page.
+//
+N.wire.on('navigate.done', function navbar_menu_change_active(target) {
+  var targetPath = target.apiPath.split('.'), tabs, active;
+
+  tabs = $('.nav-vert__overlay').find('[data-api-path]');
+  tabs.removeClass('active');
+
+  // Select the most specific tab - with the longest API path match.
+  active = _.max(tabs, function (tab) {
+    var tabPath = $(tab).data('apiPath').split('.')
+      , index   = -1
+      , length  = Math.min(tabPath.length, targetPath.length);
+
+    do { index += 1; }
+    while (index < length && tabPath[index] === targetPath[index]);
+
+    return index;
+  });
+
+  // if autoselection not disabled - add highlighting class
+  if ($(active).data('autoselect') !== 0) {
+    $(active).addClass('active');
+  }
+});
