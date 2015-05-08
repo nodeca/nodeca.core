2a058c2da475baf1ca8c9d87b173af0aa7a44540
Alex Kocharin <alex@kocharin.ru>
2015-05-08 14:44:17 +0000

    Create and dynamically update meta tags

---
+++ client/common/layout/meta/meta.js
@@ -0,0 +1,30 @@
+// Update meta tags when user moves to a new page
+//
+
+'use strict';
+
+
+N.wire.on('navigate.done', function update_meta(data) {
+  if (!data.locals) {
+    return;
+  }
+
+  // Remove meta tags from previous page, i.e. loop through all link
+  // and meta tags and remove known ones
+  //
+  $('head meta, head link').filter(function (n, el) {
+    var $el = $(el);
+
+    if ($el.prop('tagName') === 'LINK') {
+      return ($el.attr('rel') || '').match(/^(canonical|prev|next)$/i);
+    }
+
+    return ($el.attr('name') || '').match(/^(title|description|keywords)$/i) ||
+           ($el.attr('property') || '').match(/^og:/i) ||
+           ($el.attr('name') || '').match(/^twitter:/i);
+  }).remove();
+
+  // Add meta tags for this page
+  //
+  $('head').append($(N.runtime.render(module.apiPath, data.locals)));
+});
