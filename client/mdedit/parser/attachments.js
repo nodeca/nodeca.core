f775f8e8d4ac469c3a0a5b7edad2faae8030a09d
Alex Kocharin <alex@kocharin.ru>
2015-05-22 15:31:00 +0000

    Add a possibility to resize an attach

--- client/mdedit/parser/attachments.js
+++ client/mdedit/parser/attachments.js
@@ -3,6 +3,9 @@
 N.wire.once('init:parser', function attachments_plugin_init() {
   N.parse.addPlugin(
     'attachments',
-    require('nodeca.core/lib/parser/plugins/attachments')(N, '$$ JSON.stringify(N.models.users.MediaInfo.types) $$')
+    require('nodeca.core/lib/parser/plugins/attachments')(N, {
+      types: '$$ JSON.stringify(N.models.users.MediaInfo.types) $$',
+      sizes: '$$ JSON.stringify(N.config.users.uploads.resize) $$'
+    })
   );
 });
