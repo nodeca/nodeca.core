03f8186bf0d59788ff3e742919df328879545646
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-05-23 02:06:20 +0300

    Spoiler: tweaked style & animation

--- client/common/blocks/markup/spoiler/spoiler.js
+++ client/common/blocks/markup/spoiler/spoiler.js
@@ -7,14 +7,6 @@
 N.wire.once('navigate.done', function () {
 
   $(document).on('click', '.spoiler__title', function (event) {
-    var spoiler = $(this).parent('.spoiler');
-
-    if (spoiler.hasClass('spoiler__m-open')) {
-      spoiler.removeClass('spoiler__m-open');
-      spoiler.children('.spoiler__content').show().stop().hide('fast');
-    } else {
-      spoiler.addClass('spoiler__m-open');
-      spoiler.children('.spoiler__content').hide().stop().show('fast');
-    }
+    $(this).parent('.spoiler').toggleClass('spoiler__m-open');
   });
 });
