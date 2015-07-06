68ae0f7776ec4560c8ff2a0652b49c372596c2a2
Alex Kocharin <alex@kocharin.ru>
2015-07-06 14:41:26 +0000

    Add a shortcut dispatcher

--- client/admin/core/system/event_actions/event_actions.js
+++ client/admin/core/system/event_actions/event_actions.js
@@ -91,4 +91,125 @@ N.wire.once('navigate.done', function () {
     handleAction(apiPath, data);
     event.preventDefault();
   });
+
+
+  // Whenever a key is pressed, this function:
+  //
+  //  1. traverses DOM up to the root and executes respective wire functions
+  //     defined in `data-keymap` attributes (stops at `data-keymap-reset`)
+  //
+  //  2. when the top-level is reached, it executes a wire function defined
+  //     in #content[data-keymap] (this allows pages to define local
+  //     shortcuts in their layout
+  //
+  //  3. after that, if nothing stops event propagation,
+  //     `event.keypress.escape` event is emitted if the key is ESC
+  //
+  $(document).on('keydown', 'html, [data-keymap], [data-keymap-reset]', function (event) {
+    // Prevent shortcuts from triggering inside text areas, unless keymap-reset
+    // flag is specified, this function is similar to:
+    // https://craig.is/killing/mice#api.stopCallback
+    //
+    if (event.target !== this) {
+      var target = event.target;
+
+      if (target.tagName === 'INPUT' ||
+          target.tagName === 'SELECT' ||
+          target.tagName === 'TEXTAREA' ||
+          (target.contentEditable && target.contentEditable === 'true') ||
+          $(target).hasClass('modal') ||
+          $(target).parents('.modal').length) {
+
+        if ($(target).data('keymap-reset') !== false) {
+          return;
+        }
+      }
+    }
+
+    if ($(this).attr('id') === 'content') {
+      // #content(data-keymap) is delegated to html tag,
+      // so exclude it from normal propagation chain to avoid double calls
+      return;
+    }
+
+    var code_to_text = {
+      8: 'backspace',
+      9: 'tab',
+      13: 'enter',
+      16: 'shift',
+      17: 'ctrl',
+      18: 'alt',
+      20: 'capslock',
+      27: 'escape',
+      32: 'space',
+      33: 'pageup',
+      34: 'pagedown',
+      35: 'end',
+      36: 'home',
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down',
+      45: 'insert',
+      46: 'delete',
+      91: 'meta',
+      93: 'meta',
+      224: 'meta'
+    };
+
+    var data = {
+      // handlers can set it to "true" to allow default browser behavior
+      allow_default:     false,
+
+      // handlers can set it to "true" to allow other handlers to invoke
+      allow_propagation: false,
+
+      event:             event,
+      $this:             $(this)
+    };
+
+    var key = '';
+
+    if (event.ctrlKey)  { key += 'ctrl+'; }
+    if (event.altKey)   { key += 'alt+'; }
+    if (event.shiftKey) { key += 'shift+'; }
+    if (event.metaKey)  { key += 'meta+'; }
+
+    if (code_to_text[event.which]) {
+      key += code_to_text[event.which];
+    } else {
+      key += String.fromCharCode(event.which).toLowerCase();
+    }
+
+    var map;
+
+    if (this.tagName === 'HTML') {
+      // use #content keymap instead of html's own keymap,
+      // this allows pages to specify shortcuts in their layout
+      //
+      map = $('#content').data('keymap');
+    } else {
+      map = data.$this.data('keymap');
+    }
+
+    if (map && Object.prototype.hasOwnProperty.call(map, key)) {
+      var apiPath = map[key];
+
+      handleAction(apiPath, data);
+    } else {
+      var reset = data.$this.data('keymap-reset');
+
+      // allow both `<div data-keymap-reset>` and `<div data-keymap-reset=true>`
+      data.allow_propagation = !(reset || reset === '');
+      data.allow_default     = true;
+    }
+
+    // emit special event for pressing ESC key
+    if (this.tagName === 'HTML' && data.allow_propagation && key === 'escape') {
+      handleAction('event.keypress.escape', data);
+    }
+
+    if (!data.allow_default)     { event.preventDefault(); }
+    if (!data.allow_propagation) { event.stopPropagation(); }
+  });
 });
