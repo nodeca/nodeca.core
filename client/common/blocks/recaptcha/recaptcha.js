837be0ec2b96d3a583941dc31d9b68f682b8b8d0
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-10-18 17:29:44 +0300

    Improved recaptcha loading

--- client/common/blocks/recaptcha/recaptcha.js
+++ client/common/blocks/recaptcha/recaptcha.js
@@ -1,31 +1,47 @@
 'use strict';
 
 
+function loadRecaptchaScript(callback) {
+  if (window.grecaptcha) {
+    callback();
+    return;
+  }
+
+  var cb_name = 'rc_cb' + Math.floor(Math.random() * 1000000000000);
+
+  // handler to init captcha on script load
+  window[cb_name] = function onLoad() {
+    delete window[cb_name];
+    callback();
+  };
+
+  var rc_url = 'https://www.google.com/recaptcha/api.js?render=explicit&onload=' + cb_name;
+
+  $.getScript(rc_url)
+    .fail(function () {
+      delete window[cb_name];
+      callback(new Error('Cannot load ReCaptcha script.'));
+    });
+}
+
+
 N.wire.on(module.apiPath + '.create', function recaptcha_create(__, callback) {
 
-  var rc_url = 'https://www.google.com/recaptcha/api.js?render=explicit';
   var rc_id  = 'recaptcha_div';
   var rc_options = {
     sitekey: N.runtime.recaptcha.public_key,
     theme: 'light'
   };
 
-  if (window.grecaptcha) {
+  loadRecaptchaScript(function (err) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
     window.grecaptcha.render(rc_id, rc_options);
     callback();
-    return;
-  }
-
-  $.getScript(rc_url)
-    .done(function () {
-      setTimeout(function () {
-        window.grecaptcha.render(rc_id, rc_options);
-        callback();
-      }, 1000);
-    })
-    .fail(function () {
-      callback(new Error('Cannot load ReCaptcha script.'));
-    });
+  });
 });
 
 
