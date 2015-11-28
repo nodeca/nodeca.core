01a56091daa0319f3d3ba821e7eddde6cb24d37f
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-11-28 15:59:20 +0300

    Send pings via task queue

--- server/admin/core/dashboard/dashboard.js
+++ server/admin/core/dashboard/dashboard.js
@@ -4,10 +4,6 @@
 module.exports = function (N, apiPath) {
   N.validate(apiPath, {});
 
-  setInterval(function () {
-    N.live.emit('admin.ping', {});
-  }, 1000);
-
   N.wire.on(apiPath, function dashboard(env, next) {
     env.res.head.title = env.t('title');
     next();
