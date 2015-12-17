7a4cf20f9324fdbeec3c79abe65e934c1ae7dc5d
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-12-17 09:46:56 +0300

    Removed orphane dependency

--- assets/main_common.js
+++ assets/main_common.js
@@ -1,5 +1,4 @@
 //= require vendor/powertip/jquery.powertip
-//= require vendor/jquery-ui/ui/jquery.ui.widget.js
 //= require vendor/blueimp-canvas-to-blob/js/canvas-to-blob.min.js
 //= require frontend/bootstrap_custom
 //= require frontend/js/bootstrap_collapse_ext
