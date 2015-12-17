25d68159770d167303872a7e924aaf9bd7046bf7
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-12-17 09:38:15 +0300

    Added missed file

---
+++ client/lib-admin/init.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+// require('jquery-ui/ui/jquery.ui.core');
+// require('jquery-ui/ui/jquery.ui.widget');
+// require('jquery-ui/ui/jquery.ui.mouse');
+// require('jquery-ui/ui/jquery.ui.position');
+// require('jquery-ui/ui/jquery.ui.draggable');
+// require('jquery-ui/ui/jquery.ui.droppable');
+// require('jquery-ui/ui/jquery.ui.menu');
+// require('jquery-ui/ui/jquery.ui.autocomplete');
+
+// For nestedSortable
+require('jquery-ui/sortable');
+
+// Used to sort forums
+require('./_vendor/jquery.mjs.nestedSortable');
