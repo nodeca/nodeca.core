c4a114801447632ca971fd291a4a927957cd626c
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-12-17 09:13:08 +0300

    Loader: partially moved admin libs to separate pcg to simplify init

--- assets/main_admin.js
+++ assets/main_admin.js
@@ -1,13 +1,3 @@
 //= require backend/bootstrap_custom
-//= require vendor/jquery-ui/ui/jquery.ui.core
-//= require vendor/jquery-ui/ui/jquery.ui.widget
-//= require vendor/jquery-ui/ui/jquery.ui.mouse
-//= require vendor/jquery-ui/ui/jquery.ui.position
-//= require vendor/jquery-ui/ui/jquery.ui.draggable
-//= require vendor/jquery-ui/ui/jquery.ui.droppable
-//= require vendor/jquery-ui/ui/jquery.ui.menu
-//= require vendor/jquery-ui/ui/jquery.ui.autocomplete
-//= require vendor/jquery-ui/ui/jquery.ui.sortable
 //= require vendor/typeahead.js/dist/typeahead.bundle
-//= require backend/js/jquery.mjs.nestedSortable
 //= require client
