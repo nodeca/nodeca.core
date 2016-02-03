3867a88d5d08b29366b7b7821dfc3ce31433c5e5
Kirill Efimov <kirill89@gmail.com>
2016-02-03 12:38:24 +0300

    Rewrite admin settings page to promises

--- client/admin/core/global_settings/edit.js
+++ client/admin/core/global_settings/edit.js
@@ -1,22 +1,22 @@
 'use strict';
 
 
-var _  = require('lodash');
-var ko = require('knockout');
+const _  = require('lodash');
+const ko = require('knockout');
 
 
 // Module variables, initialized at navigate.done
-var categoryKeys     = null;
-var categoryNames    = null;
-var categorySettings = null;
-var settingModels    = null;
-var isDirty          = null;
+let categoryKeys     = null;
+let categoryNames    = null;
+let categorySettings = null;
+let settingModels    = null;
+let isDirty          = null;
 
 
 function submit() {
-  var payload = {};
+  let payload = {};
 
-  settingModels.forEach(function (setting) {
+  settingModels.forEach(setting => {
     if (setting.value.isDirty()) {
       if (setting.type === 'number') {
         payload[setting.name] = Number(setting.value());
@@ -26,16 +26,16 @@ function submit() {
     }
   });
 
-  N.io.rpc('admin.core.global_settings.update', { settings: payload }).then(function () {
+  N.io.rpc('admin.core.global_settings.update', { settings: payload }).then(() => {
     settingModels.forEach(function (setting) { setting.value.markClean(); });
 
-    N.wire.emit('notify', { type: 'info', message: t('saved') });
-  });
+    return N.wire.emit('notify', { type: 'info', message: t('saved') });
+  }).catch(err => N.wire.emit('error', err));
 }
 
 
 function SettingModel(name, schema, value) {
-  var tName = '@admin.core.setting_names.' + name,
+  let tName = '@admin.core.setting_names.' + name,
       tHelp = '@admin.core.setting_names.' + name + '_help';
 
   this.id            = 'setting_' + name;
@@ -45,13 +45,11 @@ function SettingModel(name, schema, value) {
   this.localizedName = t(tName);
   this.localizedHelp = t.exists(tHelp) ? t(tHelp) : '';
 
-  this.valueOptions = _.map(schema.values, function (option) {
-    return {
-      name:  option.name,
-      value: option.value,
-      title: t.exists('@' + option.title) ? t('@' + option.title) : option.name
-    };
-  });
+  this.valueOptions = _.map(schema.values, option => ({
+    name: option.name,
+    value: option.value,
+    title: t.exists('@' + option.title) ? t('@' + option.title) : option.name
+  }));
 
   if (schema.type === 'combobox') {
     this.value = ko.observableArray(value).extend({ dirty: false });
@@ -62,7 +60,7 @@ function SettingModel(name, schema, value) {
 
 
 N.wire.on('navigate.done:' + module.apiPath, function global_settings_edit_init() {
-  var inputSchemas = N.runtime.page_data.setting_schemas,
+  let inputSchemas = N.runtime.page_data.setting_schemas,
       inputValues  = N.runtime.page_data.setting_values;
 
   // Initialize module variables.
@@ -73,7 +71,7 @@ N.wire.on('navigate.done:' + module.apiPath, function global_settings_edit_init(
 
   // Collect category keys and prepare setting models.
   _.forEach(inputSchemas, function (schema, name) {
-    var key   = schema.category_key,
+    let key   = schema.category_key,
         model = new SettingModel(name, schema, inputValues[name]);
 
     if (categoryKeys.indexOf(key) < 0) {
@@ -90,19 +88,17 @@ N.wire.on('navigate.done:' + module.apiPath, function global_settings_edit_init(
   });
 
   // Sort categories using category setting priorities.
-  categoryKeys = categoryKeys.sort(function (key) {
-    var priority = 0;
+  categoryKeys = categoryKeys.sort(key => {
+    let priority = 0;
 
-    _.forEach(categorySettings[key], function (setting) {
-      priority += setting.priority;
-    });
+    _.forEach(categorySettings[key], setting => { priority += setting.priority; });
 
     return -priority;
   });
 
   // Sort settings within categories.
-  _.forEach(categorySettings, function (settings, key) {
-    categorySettings[key] = settings.sort(function (a, b) {
+  _.forEach(categorySettings, (settings, key) => {
+    categorySettings[key] = settings.sort((a, b) => {
       if (a.priority === b.priority) {
         return a.name.localeCompare(b.name);
       }
@@ -110,11 +106,7 @@ N.wire.on('navigate.done:' + module.apiPath, function global_settings_edit_init(
     });
   });
 
-  isDirty = ko.computed(function () {
-    return _.some(settingModels, function (setting) {
-      return setting.value.isDirty();
-    });
-  });
+  isDirty = ko.computed(() => _.some(settingModels, setting => setting.value.isDirty()));
 
   ko.applyBindings({
     categoryKeys,
