e448fdee3e212df0c9bac5e752d58b48d68fcb5f
Kirill Efimov <kirill89@gmail.com>
2016-02-18 20:07:13 +0300

    Added abuse report dlg

---
+++ client/common/blocks/abuse_report_dlg/abuse_report_dlg.js
@@ -0,0 +1,69 @@
+// Popup dialog to report moderator
+//
+// - messages - array of messages string (first line is title)
+//
+'use strict';
+
+
+let $dialog;
+let params;
+let result;
+
+
+N.wire.once(module.apiPath, function init_handlers() {
+
+  // Submit button handler
+  //
+  N.wire.on(module.apiPath + ':submit', function submit_abuse_report_dlg(form) {
+    params.message = form.fields.message;
+    result = params;
+    $dialog.modal('hide');
+  });
+
+
+  // Template select handler
+  //
+  N.wire.on(module.apiPath + ':quick_fill', function template_select_abuse_report_dlg(data) {
+    let value = data.$this.val();
+
+    if (value) $dialog.find('.abuse-report-dlg__message').val(value);
+  });
+
+
+  // Close dialog on sudden page exit (if user click back button in browser)
+  //
+  N.wire.on('navigate.exit', function teardown_page() {
+    if ($dialog) {
+      $dialog.modal('hide');
+    }
+  });
+});
+
+
+// Init dialog
+//
+N.wire.on(module.apiPath, function show_abuse_report_dlg(options) {
+  params = options;
+  $dialog = $(N.runtime.render(module.apiPath, params));
+
+  $('body').append($dialog);
+
+  return new Promise((resolve, reject) => {
+    $dialog
+      .on('shown.bs.modal', () => {
+        $dialog.find('.btn-default').focus();
+      })
+      .on('hidden.bs.modal', () => {
+        // When dialog closes - remove it from body and free resources
+        $dialog.remove();
+        $dialog = null;
+        params = null;
+
+        if (result) resolve(result);
+        else reject('CANCELED');
+
+        result = null;
+      })
+      .modal('show');
+  });
+});
