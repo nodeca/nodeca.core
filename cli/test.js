219348edd0e1a18d60544d56bb459a8d0ad385a3
Alex Kocharin <alex@kocharin.ru>
2016-04-22 11:17:06 +0000

    Split cluster initialization into hooks

--- cli/test.js
+++ cli/test.js
@@ -60,7 +60,7 @@ module.exports.run = function (N, args) {
     yield Promise.resolve()
       .then(() => N.wire.emit('init:models', N))
       .then(() => N.wire.emit('init:bundle', N))
-      .then(() => N.wire.emit('init:server', N))
+      .then(() => N.wire.emit('init:services', N))
       .then(() => N.wire.emit('init:tests', N));
 
     let mocha        = new Mocha({ timeout: 20000 });
