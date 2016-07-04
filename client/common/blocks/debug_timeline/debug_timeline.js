10b16d083abadf25bf154718be488a4f1929e074
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-07-04 20:10:44 +0300

    Updated debug timeline style

--- client/common/blocks/debug_timeline/debug_timeline.js
+++ client/common/blocks/debug_timeline/debug_timeline.js
@@ -28,24 +28,3 @@ N.wire.on('io.complete', function debug_timeline(info) {
 N.wire.on('common.blocks.debug_timeline:toggle_hidden', function debug_timeline_toggle_hidden(data) {
   data.$this.toggleClass('debug-timeline__m-show-hidden');
 });
-
-
-///////////////////////////////////////////////////////////////////////////////
-// Show debug timeline by `alt+ctrl+d` hotkey:
-//
-// - add hotkey handler on every page
-// - toggle class `debug-on` on body by hotkey
-//
-
-N.wire.on('navigate.done', function debug_timeline_init_hotkey() {
-  let $content = $('#content');
-  let keymap = $content.data('keymap') || {};
-
-  keymap['alt+ctrl+d'] = 'common.blocks.debug_timeline:show';
-  $content.data('keymap', keymap);
-});
-
-
-N.wire.on('common.blocks.debug_timeline:show', function debug_timeline_hotkey_toggle() {
-  $('body').toggleClass('debug-on');
-});
---
+++ client/common/layout/debug_toggle.js
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+// Show debug timeline by `alt+ctrl+d` hotkey:
+//
+// - add hotkey handler on every page
+// - toggle class `debug-on` on body by hotkey
+//
+
+N.wire.on('navigate.done', function debug_timeline_init_hotkey() {
+  let $content = $('#content');
+  let keymap = $content.data('keymap') || {};
+
+  keymap['alt+ctrl+d'] = '@debug:toggle';
+  $content.data('keymap', keymap);
+});
+
+
+N.wire.on('@debug:toggle', function debug_timeline_hotkey_toggle() {
+  $('body').toggleClass('debug-on');
+});
