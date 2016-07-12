6a3859042afe28a8cd2f65b2738597381dac5bac
Kirill Efimov <kirill89@gmail.com>
2016-07-12 16:14:12 +0300

    nestedSortable -> nestable

--- client/lib-admin/init.js
+++ client/lib-admin/init.js
@@ -1,17 +1,5 @@
 'use strict';
 
 
-// require('jquery-ui/ui/jquery.ui.core');
-// require('jquery-ui/ui/jquery.ui.widget');
-// require('jquery-ui/ui/jquery.ui.mouse');
-// require('jquery-ui/ui/jquery.ui.position');
-// require('jquery-ui/ui/jquery.ui.draggable');
-// require('jquery-ui/ui/jquery.ui.droppable');
-// require('jquery-ui/ui/jquery.ui.menu');
-// require('jquery-ui/ui/jquery.ui.autocomplete');
-
-// For nestedSortable
-require('jquery-ui/sortable');
-
 // Used to sort forums
-require('./_vendor/jquery.mjs.nestedSortable');
+require('./_vendor/jquery.nestable');
