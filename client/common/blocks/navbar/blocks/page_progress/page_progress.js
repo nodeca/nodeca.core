6ebd78479ae88a86c3f1c86f4d3d883f1a8f88eb
Alex Kocharin <alex@kocharin.ru>
2016-08-22 17:29:49 +0000

    Make first/last buttons in navbar grey on page boundaries

--- client/common/blocks/navbar/blocks/page_progress/page_progress.js
+++ client/common/blocks/navbar/blocks/page_progress/page_progress.js
@@ -35,10 +35,60 @@ N.wire.on(module.apiPath + ':update', function update_progress(data) {
   }
 
   if (data.link_top) {
-    $('.page-progress__button-top').attr('href', data.link_top);
+    $('.page-progress__button-first').attr('href', data.link_top);
   }
 
   if (data.link_bottom) {
-    $('.page-progress__button-bottom').attr('href', data.link_bottom);
+    $('.page-progress__button-last').attr('href', data.link_bottom);
   }
+
+  // Hide upward arrow if:
+  //  - user sees the top of the page
+  //
+  // Hide downward arrow if:
+  //  - user sees the bottom of the page (e.g. 39/42 and 3 last posts are all visible)
+  //  - user is positioned at the last post (e.g. 42/42)
+  //
+  if (current >= total) {
+    $('.page-progress__button-last').addClass('page-progress__m-disabled');
+  } else {
+    $('.page-progress__button-last').removeClass('page-progress__m-disabled');
+  }
+});
+
+
+// Whenever user clicks on the upward arrow:
+//  - if url is different, navigate to it
+//  - if url is the same, scroll to the top
+//
+N.wire.on(module.apiPath + ':nav_to_top', function navigate_to_top(data) {
+  let href = data.$this.prop('href');
+
+  if (href === window.location.href) {
+    $(window).scrollTop(0);
+    return;
+  }
+
+  return N.wire.emit('navigate.to', href);
+});
+
+
+// Add a handler on downward arrow to assure we have similar behavior of those
+// two buttons (e.g. ctrl+click is treated the same as click on both)
+//
+N.wire.on(module.apiPath + ':nav_to_bottom', function navigate_to_bottom(data) {
+  let href = data.$this.prop('href');
+
+  return N.wire.emit('navigate.to', href);
+});
+
+
+// Inject affix plugin config to track when user scrolls page to the
+// top. Toggle content-top class if that happens.
+//
+N.wire.before('navigate.done', function add_content_affix() {
+  $('#content')
+    .addClass('_affix')
+    .data('affix-top',    { class_above: 'content-top' })
+    .data('affix-bottom', { class_below: 'content-bottom' });
 });
