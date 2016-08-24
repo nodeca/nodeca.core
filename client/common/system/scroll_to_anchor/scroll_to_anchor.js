a8c8dd165a8b8c0e221a2b9e6c8eb3e28c506362
Alex Kocharin <alex@kocharin.ru>
2016-08-24 14:58:24 +0000

    Calculate navbar height as body margin + body padding

--- client/common/system/scroll_to_anchor/scroll_to_anchor.js
+++ client/common/system/scroll_to_anchor/scroll_to_anchor.js
@@ -3,6 +3,8 @@
 
 'use strict';
 
+const navbarHeight = parseInt($('body').css('margin-top'), 10) + parseInt($('body').css('padding-top'), 10);
+
 
 N.wire.on('navigate.done:*', { priority: 990 }, function scroll_to_anchor(data) {
   if (data.no_scroll || !data.anchor) return;
@@ -12,5 +14,5 @@ N.wire.on('navigate.done:*', { priority: 990 }, function scroll_to_anchor(data) 
   if (!el.length) return;
 
   data.no_scroll = true;
-  $(window).scrollTop(el.offset().top - $('.navbar').height());
+  $(window).scrollTop(el.offset().top - navbarHeight);
 });
