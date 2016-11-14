4f5a2682cfddeb2ed38106bb10de979ab4137787
Alex Kocharin <alex@kocharin.ru>
2016-11-14 12:23:24 +0000

    Fix location dialog

--- client/admin/core/blocks/location_show_dlg/location_show_dlg.js
+++ client/admin/core/blocks/location_show_dlg/location_show_dlg.js
@@ -9,7 +9,10 @@ let map;
 
 // Show dialog
 //
-N.wire.before(module.apiPath, function location_show_dlg(data) {
+N.wire.on(module.apiPath, function location_show_dlg(data) {
+  //
+  // Create dialog
+  //
   let params = {
     latitude:  data.$this.data('latitude'),
     longitude: data.$this.data('longitude')
@@ -18,6 +21,24 @@ N.wire.before(module.apiPath, function location_show_dlg(data) {
   $dialog = $(N.runtime.render(module.apiPath, params));
   $('body').append($dialog);
 
+  //
+  // Load leaflet and init map afterwards
+  //
+  let waitForMap = N.loader.loadAssets('vendor.leaflet').then(function init_map() {
+    const leaflet = require('leaflet').noConflict();
+
+    // dialog had been closed before leaflet was loaded
+    if (!$dialog) return;
+
+    // rare case when user opens dialog, then closes, and opens it again quickly
+    if (map) map.remove();
+
+    map = leaflet.map($('.location-show-dlg__map')[0]).setView([ 20, 0 ], 2);
+  });
+
+  //
+  // Start tiling map after dialog is fully shown
+  //
   $dialog
     .on('hidden.bs.modal', function () {
       // When dialog closes - remove it from body and free resources.
@@ -29,49 +50,20 @@ N.wire.before(module.apiPath, function location_show_dlg(data) {
         map = null;
       }
     })
+    .on('shown.bs.modal', function () {
+      waitForMap.then(function tile_map() {
+        const leaflet = require('leaflet').noConflict();
+
+        // map is initialized when container size is 0,
+        // so we need to reset internal size cache to actual size
+        map.invalidateSize();
+
+        leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
+        }).addTo(map);
+      });
+    })
     .modal('show');
-});
-
-
-// Load dependencies
-//
-N.wire.before(module.apiPath, function load_deps() {
-  return N.loader.loadAssets('vendor.leaflet');
-});
-
-
-function tile_map() {
-  const leaflet = require('leaflet').noConflict();
-
-  // map is created, then dialog is animated, then tiles are rendered,
-  // so we need to reset position cache filled on map creation
-  map.invalidateSize();
-
-  leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
-  }).addTo(map);
-}
-
-
-// Render map
-//
-N.wire.on(module.apiPath, function render_map(/* data */) {
-  const leaflet = require('leaflet').noConflict();
-
-  // dialog had been closed before leaflet was loaded
-  if (!$dialog) return;
-
-  // rare case when user opens dialog, then closes, and opens it again quickly
-  if (map) map.remove();
-
-  map = leaflet.map($('.location-show-dlg__map')[0]).setView([ 20, 0 ], 2);
-
-  if ($dialog.hasClass('in')) {
-    tile_map();
-  } else {
-    // delay map rendering until dialog animation is completed
-    $dialog.on('shown.bs.modal', tile_map);
-  }
 });
 
 
