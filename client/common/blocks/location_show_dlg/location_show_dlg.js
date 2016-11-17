4b9360b7bb3be09db9a91740e8952d1679862e1d
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-11-17 22:14:03 +0300

    Use https for OSM access

--- client/common/blocks/location_show_dlg/location_show_dlg.js
+++ client/common/blocks/location_show_dlg/location_show_dlg.js
@@ -80,7 +80,7 @@ N.wire.on(module.apiPath, function location_show_dlg(data) {
         // so we need to reset internal size cache to actual size
         map.invalidateSize();
 
-        leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
         }).addTo(map);
       });
