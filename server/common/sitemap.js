01c4ef4cf016fba53004f5792809861b1bd4f00f
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-11-17 15:06:56 +0300

    sitemap: code cleanup

--- server/common/sitemap.js
+++ server/common/sitemap.js
@@ -14,27 +14,24 @@ module.exports = function (N, apiPath) {
   N.wire.on(apiPath, function* print_sitemap_index(env) {
     let sitemap = yield N.models.core.SiteMap.findOne({ active: true }).sort('-_id');
 
-    env.body = env.body || '';
-    env.headers['Content-Type'] = 'text/xml';
-
-    env.body +=
-      '<?xml version="1.0" encoding="UTF-8"?>\n' +
-      '<sitemapindex\n' +
-      '  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n' +
-      '  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' +
-      '  xsi:schemaLocation="\n' +
-      '    http://www.sitemaps.org/schemas/sitemap/0.9' +
-      '    http://www.sitemaps.org/schemas/sitemap/09/siteindex.xsd">\n';
-
-    if (sitemap) {
-      env.body += sitemap.files.map(file =>
+    let content = !sitemap ? '' : sitemap.files.map(file =>
         '<sitemap>\n' +
         `  <loc>${N.router.linkTo('core.gridfs_tmp', { bucket: file })}</loc>\n` +
         `  <lastmod>${sitemap._id.getTimestamp().toISOString()}</lastmod>\n` +
         '</sitemap>\n').join('');
-    }
 
-    env.body += '</sitemapindex>\n';
+    env.headers['Content-Type'] = 'text/xml';
+
+    /* eslint-disable max-len */
+    env.body = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/09/siteindex.xsd">
+${content}
+</sitemapindex>
+`;
+
   });
 
 };
