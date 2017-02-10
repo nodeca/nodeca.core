645bc731624a8c11befd65afd1e101884dbd4ce0
Alex Kocharin <alex@kocharin.ru>
2017-02-10 19:35:23 +0000

    Run common.embed for all links, not just autolinks

--- client/mdedit/parser/link_expand.js
+++ client/mdedit/parser/link_expand.js
@@ -7,18 +7,19 @@ N.wire.once('init:parser', function link_expand_plugin_init() {
       parser.bus.on('ast2html', function expand_links(data) {
         if (!data.params.rpc_cache) return;
 
-        data.ast.find('msg-link[data-nd-auto]').each(function () {
-          var $tag  = $(this);
-          var url   = $tag.attr('href');
-          var types = data.params.options.link_to_title ? [ 'inline' ] : [];
+        data.ast.find('msg-link').each(function () {
+          let $tag    = $(this);
+          let url     = $tag.attr('href');
+          let types   = data.params.options.link_to_title ? [ 'inline' ] : [];
+          let is_auto = $tag.data('nd-auto');
 
-          if (($tag.parent().prop('tagName') === 'P') && ($tag.parent().contents().length === 1)) {
+          if (is_auto && ($tag.parent().prop('tagName') === 'P') && ($tag.parent().contents().length === 1)) {
             if (data.params.options.link_to_snippet) {
               types.unshift('block');
             }
           }
 
-          var result = data.params.rpc_cache.get('common.embed', { url, types });
+          let result = data.params.rpc_cache.get('common.embed', { url, types });
 
           // Switch url destination if it's been unshortened
           if (result && result.canonical) {
@@ -27,16 +28,18 @@ N.wire.once('init:parser', function link_expand_plugin_init() {
 
           if (!result || !result.html) return;
 
-          var replacement = $(result.html);
+          let replacement = $(result.html);
 
           replacement.attr('data-nd-orig', url);
 
-          if (result.type === 'block') {
-            // if result is a block, replace parent `P` tag
-            $tag.parent().replaceWith(replacement);
-          } else {
-            // otherwise replace `A` tag itself
-            $tag.replaceWith(replacement);
+          if (is_auto) {
+            if (result.type === 'block') {
+              // if result is a block, replace parent `P` tag
+              $tag.parent().replaceWith(replacement);
+            } else {
+              // otherwise replace `A` tag itself
+              $tag.replaceWith(replacement);
+            }
           }
         });
       });
