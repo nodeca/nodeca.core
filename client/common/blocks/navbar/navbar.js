f73a3d8f40284104114fc4be7e0bdd96bd12aed1
Alex Kocharin <alex@kocharin.ru>
2019-07-02 18:41:09 +0300

    Implement scrollable list

--- client/common/blocks/navbar/navbar.js
+++ client/common/blocks/navbar/navbar.js
@@ -40,3 +40,42 @@ N.wire.on('navigate.to', function navbar_menu_minimize() {
   // $('.navbar .navbar-collapse').collapse('hide');
   $('.navbar .navbar-collapse').removeClass('show');
 });
+
+
+// When user scrolls the page, show secondary navbar if user scrolls
+// past page head.
+//
+let scroll_handler = null;
+let navbar_height = parseInt($('body').css('margin-top'), 10) + parseInt($('body').css('padding-top'), 10);
+
+
+N.wire.after('navigate.done', function secondary_navbar_show() {
+  if (document.getElementsByClassName('page-head').length === 0) return;
+  if (document.getElementsByClassName('navbar-alt').length === 0) return;
+
+  scroll_handler = _.debounce(function update_progress_on_scroll() {
+    let head = document.getElementsByClassName('page-head');
+
+    if (head.length && head[0].getBoundingClientRect().bottom > navbar_height) {
+      $('.navbar').removeClass('navbar__m-secondary');
+    } else {
+      $('.navbar').addClass('navbar__m-secondary');
+    }
+  }, 100, { maxWait: 100 });
+
+  // avoid executing it on first tick because of possible initial scrollTop()
+  setTimeout(() => {
+    $(window).on('scroll', scroll_handler);
+  }, 1);
+
+  // execute it once on page load
+  scroll_handler();
+});
+
+
+N.wire.on('navigate.exit', function progress_updater_teardown() {
+  if (!scroll_handler) return;
+  scroll_handler.cancel();
+  $(window).off('scroll', scroll_handler);
+  scroll_handler = null;
+});
