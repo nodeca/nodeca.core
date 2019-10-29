404f561b54fde7bdce9563e850cfddfac8a3676c
Alex Kocharin <alex@kocharin.ru>
2019-10-29 13:08:23 +0300

    Implement a cut md plugin for blog entries

--- client/mdedit/parser/cut.js
+++ client/mdedit/parser/cut.js
@@ -5,4 +5,25 @@ N.wire.once('init:parser', function cut_plugin_init() {
     'cut',
     require('nodeca.core/lib/parser/plugins/cut')(N)
   );
+
+  N.parser.addPlugin(
+    'cut:render',
+    function cut_render_init(parser) {
+      // default renderer
+      parser.bus.after('ast2html', function render_cut(data) {
+        data.ast.children('msg-cut').first().replaceWith('<hr class="mdedit__preview-cut">');
+        data.ast.find('msg-cut').remove();
+      });
+
+      // remove it in preview
+      parser.bus.after('ast2preview', function remove_cut(data) {
+        data.ast.find('msg-cut').remove();
+      });
+
+      // remove it when counting
+      parser.bus.after('ast2length', function remove_cut(data) {
+        data.ast.find('msg-cut').remove();
+      });
+    }
+  );
 });
