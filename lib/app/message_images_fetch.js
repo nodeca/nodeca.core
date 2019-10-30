nodeca/nodeca.core

commit 3fa50318e30200f4549cf638191eff7fe0dd78bc

Fix deprecated mongodb calls (insert, update, count, remove)

lib/app/message_images_fetch.js
@@ -222,7 +222,7 @@ module.exports = function (N, params) {
           // fetched results (i.e. error status codes). Use native method
           // to avoid auto-$set.
           //
-          await N.models.core.ImageSizeCache.collection.update(
+          await N.models.core.ImageSizeCache.collection.updateOne(
             { url },
             cache,
             { upsert: true }

internal/common/embed.js
@@ -304,7 +304,7 @@ module.exports = function (N, apiPath) {
       if (_.isEmpty(update_data[k])) delete update_data[k];
     }
 
-    await N.models.core.UrlTracker.update(
+    await N.models.core.UrlTracker.updateOne(
       { url: data.url },
       update_data,
       { upsert: false }

lib/autoload/hooks/init/models/migrations_check.js
@@ -28,7 +28,7 @@ let Migration = new Mongoose.Schema({
 Migration.statics.markPassed = function (app_name, step) {
   return this.find({ app: app_name }).then(docs => {
     if (docs.length > 0) {
-      return this.update({ app: app_name }, { $push: { steps: step } });
+      return this.updateOne({ app: app_name }, { $push: { steps: step } });
     }
 
     return this.create({ app: app_name, steps: [ step ] });
@@ -47,8 +47,8 @@ Migration.statics.updateMigrations = async function () {
     obj.app = obj._id;
     delete obj._id;
 
-    await this.collection.insert(obj);
-    await this.collection.remove({ _id: obj.app });
+    await this.collection.insertOne(obj);
+    await this.collection.deleteOne({ _id: obj.app });
   }
 
   // TODO: move to mongoose index

lib/autoload/hooks/responder/puncher.js
@@ -38,12 +38,12 @@ module.exports = function (N) {
     let secret_key = createToken();
 
     // Write puncher data to database
-    let result = await N.models.core.PuncherStats.collection.insert({
+    let result = await N.models.core.PuncherStats.collection.insertOne({
       secret_key,
       data:  JSON.stringify(env.extras.puncher.result)
     });
 
     // send stats in a cookie rather than env.res to keep body checksum constant
-    env.extras.setCookie('stats-id', `${result.insertedIds[0]}_${secret_key}`, { httpOnly: false });
+    env.extras.setCookie('stats-id', `${result.insertedId}_${secret_key}`, { httpOnly: false });
   });
 };

lib/autoload/jobs/expand_links_cache.js
@@ -28,7 +28,7 @@ module.exports = function (N) {
         // info for progress bar in admin control panel
         let uid = this.uid;
         let current = 0;
-        let total = await N.models.core.UrlTracker.count()
+        let total = await N.models.core.UrlTracker.countDocuments()
                               .where('status').equals(statuses.PENDING);
 
         let get_url_block_last_id;

lib/autoload/jobs/geo_location_resolve.js
@@ -73,7 +73,7 @@ module.exports = function (N) {
         error = err;
       }
 
-      await N.models.core.GeoOverpass.update(
+      await N.models.core.GeoOverpass.updateOne(
         { hash: req_id },
         { $set: _.omitBy({
           location:   [ lon, lat ],
@@ -168,7 +168,7 @@ module.exports = function (N) {
         error = err;
       }
 
-      await N.models.core.GeoOverpassNearest.update(
+      await N.models.core.GeoOverpassNearest.updateOne(
         { hash: req_id },
         { $set: _.omitBy({
           location:   [ lon, lat ],
@@ -260,7 +260,7 @@ module.exports = function (N) {
 
       let error = new Error(body.error);
 
-      await N.models.core.GeoNominatim.update(
+      await N.models.core.GeoNominatim.updateOne(
         { hash: req_id },
         { $set: _.omitBy({
           location:   [ lon, lat ],
@@ -362,7 +362,7 @@ module.exports = function (N) {
             continue;
           }
 
-          await N.models.core.Location.update(
+          await N.models.core.Location.updateOne(
             { hash: N.models.core.Location.hash([ lon, lat ], locale) },
             { $set: _.omitBy({
               location: [ lon, lat ],

lib/autoload/jobs/image_sizes_fetch.js
@@ -94,7 +94,7 @@ module.exports = function (N) {
         // info for progress bar in admin control panel
         let uid = this.uid;
         let current = 0;
-        let total = await N.models.core.ImageSizeCache.count()
+        let total = await N.models.core.ImageSizeCache.countDocuments()
                               .where('status').equals(statuses.PENDING);
 
         let get_url_block_last_id;

models/core/EmbedzaCache.js
@@ -61,7 +61,7 @@ module.exports = function (N, collectionName) {
       return N.redis.setexAsync('embedza:' + key, 60 * 60, JSON.stringify(value));
     }
 
-    return this.update({ key }, { value: JSON.stringify(value) }, { upsert: true });
+    return this.updateOne({ key }, { value: JSON.stringify(value) }, { upsert: true });
   };
 
 

commit 02cbd890aeb3caac9b1e165a362ff35837a4df9d

Fix incorrect function call in mongodb
bug introduced in https://github.com/nodeca/nodeca.core/commit/3fa50318e30200f4549cf638191eff7fe0dd78bc

lib/app/message_images_fetch.js
@@ -219,10 +219,9 @@ module.exports = function (N, params) {
           }
 
           // Replace old object with the new one to reset any previously
-          // fetched results (i.e. error status codes). Use native method
-          // to avoid auto-$set.
+          // fetched results (i.e. error status codes).
           //
-          await N.models.core.ImageSizeCache.collection.updateOne(
+          await N.models.core.ImageSizeCache.replaceOne(
             { url },
             cache,
             { upsert: true }