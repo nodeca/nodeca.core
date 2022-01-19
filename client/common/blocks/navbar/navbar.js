1828e0072b56e8c2cb9d05c0f797c1cca65b092e
Alex Kocharin <alex@kocharin.ru>
2022-01-19 20:29:56 +0300

    navbar: highlight active menu item on alt navbar

--- client/common/blocks/navbar/navbar.js
+++ client/common/blocks/navbar/navbar.js
@@ -7,15 +7,15 @@ const _ = require('lodash');
 // Update "active" tab of the navbar_menu when moving to another page.
 //
 N.wire.on('navigate.done', function navbar_menu_change_active(target) {
-  var targetPath = target.apiPath.split('.'), tabs, active;
+  let targetPath = target.apiPath.split('.'), tabs;
 
   tabs = $('.navbar').find('[data-api-path]');
   tabs.removeClass('show');
 
   if (!tabs.length) return;
 
-  function tab_score(tab) {
-    var tabPath = $(tab).data('apiPath').split('.'),
+  function tab_score(apiPath) {
+    let tabPath = apiPath.split('.'),
         index   = -1,
         length  = Math.min(tabPath.length, targetPath.length);
 
@@ -26,12 +26,18 @@ N.wire.on('navigate.done', function navbar_menu_change_active(target) {
   }
 
   // Select the most specific tab - with the longest API path match.
-  active = Array.from(tabs).reduce((a, b) => (tab_score(a) >= tab_score(b) ? a : b));
+  let bestPath = Array.from(tabs).map(t => $(t).data('apiPath'))
+                   .reduce((a, b) => (tab_score(a) >= tab_score(b) ? a : b));
 
-  // if autoselection not disabled - add highlighting class
-  if ($(active).data('autoselect') !== 0) {
-    // need to use either .nav-item.show or .nav-item>.nav-link.active
-    $(active).addClass('show');
+  for (let tab of Array.from(tabs)) {
+    let $tab = $(tab);
+    if ($tab.data('apiPath') !== bestPath) continue;
+
+    // if autoselection not disabled - add highlighting class
+    if ($tab.data('autoselect') !== 0) {
+      // need to use either .nav-item.show or .nav-item>.nav-link.active
+      $tab.addClass('show');
+    }
   }
 });
 
